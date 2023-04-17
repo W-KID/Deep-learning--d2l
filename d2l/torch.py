fe45513a5b8a078644bb3687ba896898517ec92b
W-KID <79567297+W-KID@users.noreply.github.com>
2023-04-17 19:51:52 +0800

    second commit-多层感知机

--- d2l/torch.py
+++ d2l/torch.py
@@ -220,9 +220,9 @@ def load_data_fashion_mnist(batch_size, resize=None):
         trans.insert(0, transforms.Resize(resize))
     trans = transforms.Compose(trans)
     mnist_train = torchvision.datasets.FashionMNIST(
-        root="../data2", train=True, transform=trans, download=False)
+        root="../data_mnist", train=True, transform=trans, download=True)
     mnist_test = torchvision.datasets.FashionMNIST(
-        root="../data2", train=False, transform=trans, download=False)
+        root="../data_mnist", train=False, transform=trans, download=True)
     return (data.DataLoader(mnist_train, batch_size, shuffle=True,
                             num_workers=get_dataloader_workers()),
             data.DataLoader(mnist_test, batch_size, shuffle=False,
